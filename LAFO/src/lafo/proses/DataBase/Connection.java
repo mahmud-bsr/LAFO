81fb27eac7a54d6aaa068312a60c41c0717b8de2
BachtiarAH <bachtiarah73@gmail.com>
2022-04-19 10:40:37 +0700

    k

--- LAFO/src/lafo/proses/DataBase/Connection.java
+++ LAFO/src/lafo/proses/DataBase/Connection.java
@@ -9,8 +9,9 @@ public class Connection {
     public static void main(String[] args) throws SQLException {
         
         //variabel for database connection
+//        String link = 
         String port = "3306";
-        String DataBase = "s2_cafe_lafo_v2";
+        String DataBase = "lafo";
         String url = "jdbc:mysql://localhost:"+port+"/"+DataBase;
         String user = "root";
         String password = "";
@@ -23,25 +24,14 @@ public class Connection {
             //Connection conn;
             //DriverManager.registerDriver(new com.mysql.jdbc.Driver());
             conn = DriverManager.getConnection(url, user, password); 
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);
-           
-            //SELECT `Id_Pegawai`, `Nama_Pegawai`, `Gender`, `Alamat`, `No_Hp`, `Status_pengguna`, `Tanggal_Terdaftar`, `kode_hak_akses` FROM `penngguna` WHERE 1
             
-            while(rs.next()){
-                System.out.println("ID = "+ rs.getString("Id_Pegawai"));
-                System.out.println("nama = "+ rs.getString("Nama_Pegawai"));
-                System.out.println("gender = "+ rs.getString("Gender"));
-                System.out.println("alamat = "+ rs.getString("Alamat"));
-                System.out.println("no Hp = "+ rs.getString("No_Hp"));
-                System.out.println("Status = "+ rs.getString("Status_pengguna"));
-                System.out.println("tanggal gabung = "+ rs.getString("Tanggal_Terdaftar"));
-                System.out.println("kode hak akases = "+ rs.getString("kode_hak_akses"));
-                
+            
+            if (conn.isValid(0)) {
+                System.out.println("koneksi berhasil");
             }
         }
         catch(Exception e){
-            System.out.println(e);
+            System.out.println("koneksi bermasalah : "+e);
         }
         finally{
             conn.close();
