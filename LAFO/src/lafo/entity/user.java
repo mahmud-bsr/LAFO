21e49f6f2881f8aa9be7a34bfb54ff8c5a135f09
BachtiarAH <86826543+BachtiarAH@users.noreply.github.com>
2022-05-06 06:52:03 +0700

    database dan entitity (not done)

---
+++ LAFO/src/lafo/entity/user.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lafo.entity;
+
+/**
+ *
+ * @author user
+ */
+public class user extends data{
+    String hakAkses;
+
+    public void setHakAkses(String hakAkses) {
+        this.hakAkses = hakAkses;
+    }
+
+    public String getHakAkses() {
+        return hakAkses;
+    }
+
+    
+}
